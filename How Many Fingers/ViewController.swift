178755d87395d332f1adfcdbe63272b913242f0e
Jack Douglas <Jack@Jacks-iMac.attlocal.net>
2015-07-19 20:44:18 -0700

    Initial Commit

---
+++ How Many Fingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  How Many Fingers
+//
+//  Created by Jack Douglas on 7/19/15.
+//  Copyright (c) 2015 NanoJack Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
